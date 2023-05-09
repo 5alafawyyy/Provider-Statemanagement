5be3cf4147ca0eee5993ef09a39dd40dc105a49f
Ahmed Khallaf <[ahmedkhallaf2098@gmail.com]>
2023-05-09 10:31:23 +0300

    ChangeNotifier and Consumer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,50 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ChangeNotifierProvider(
+      create: (context) => Model(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Providers"),
+        ),
+        body: Consumer<Model>(builder: ((context, model, child) {
+          return Column(
+            children: [
+              Center(
+                child: Text(model.name),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  model.changeName();
+                },
+                style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.blue),
+                  foregroundColor: MaterialStatePropertyAll(Colors.white),
+                ),
+                child: const Text(
+                  "Do Something",
+                ),
+              ),
+            ],
+          );
+        })),
+      ),
+    );
+  }
+}
+
+class Model extends ChangeNotifier {
+  String name = "Welcome";
+  changeName() {
+    name = "Ahmed";
+    notifyListeners();
   }
 }
